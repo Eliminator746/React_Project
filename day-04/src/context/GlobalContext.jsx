804811aa99e3de301910c2be0f0a4780939e4d21
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2024-12-19 16:29:25 +0530

    Implemented local Storage - IMP

--- day-04/src/context/GlobalContext.jsx
+++ day-04/src/context/GlobalContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 // create context
 const MovieContext=createContext();
@@ -11,21 +11,37 @@ export const MovieProvider=({children})=>{
     //Write all states here
     const [favourite,setFavourite]=useState([]);
 
+    // Store fav items in local storage
 
-    // Add fav
+    // This logic makes sure when revisiting the page or on page reload saved favorites don't get lost
+    useEffect(()=>{
+        const storedFavs = localStorage.getItem("favourite")
+        if(storedFavs)
+            setFavourite(JSON.parse(storedFavs))
+
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem("favourite",JSON.stringify(favourite))
+    },[favourite])
+    // Above logic makes sure favourite is again converted back to array, for further 
+
+    // Add Favourite
     const addToFavourite=(movie)=>{   
         setFavourite((prev)=>[...prev,movie])
     }
 
-    // Delete fav
+    // Delete Favourite
     const deleteFromFavourite=(movieId)=>{
-        favourite.filter((movie)=> movieId !== movie.id)
-        //setFavourite((prev)=>prev.filter((movie)=> movieId !== movie.id))
+
+        // favourite.filter((movie)=> movieId !== movie.id)  --> this does not update the state in React,
+        setFavourite((prev)=>prev.filter((movie)=> movieId !== movie.id))
     }
 
     // Is Favourite
-    const isFavourite=(movieId)=>{
-        return favourite.some((movie)=>movie.id === movieId)
+    const isFavourite= (movieId) => {
+
+        return favourite.some(movie=>movie.id === movieId)
     }
 
     const value={
